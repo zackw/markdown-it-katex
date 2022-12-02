dd20fa451406faa6a172d2e08fa43d72f6a1475c
Zack Weinberg <zackw@panix.com>
2022-12-02 13:56:28 -0500

    Update all dependencies.
    Notably, katex goes from 0.6.0 to 0.16.0 and (for testing purposes
    only) markdown-it from 6.0.0 to 13.0.0.  Several tests had to be
    updated as katex’s HTML rendering of many math constructs has changed,
    but no code changes are required.
    
    Fixes issues #18, #19, #21, #27. Partially addresses #16.
    Test cases added.

--- test/all.js
+++ test/all.js
@@ -13,7 +13,9 @@ testLoad(path.join(__dirname, 'fixtures/default.txt'), function(data){
 	data.fixtures.forEach(function (fixture){
 
 		/* generic test definition code using tape */
-		tape(fixture.header, function(t){
+		tape(fixture.header,
+		     { todo: fixture.header.includes(" XFAIL") },
+		     function(t){
 			t.plan(1);
 
 			var expected = fixture.second.text,
